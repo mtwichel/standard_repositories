d18138ca2b1643fe753080aa4cd0e55e734018c7
Marcus Twichel <marc.twichy@gmail.com>
2024-11-23 15:13:49 -0700

    feat: add repository caching (#5)

--- packages/standard_repositories/lib/src/standard_repositories.dart
+++ packages/standard_repositories/lib/src/standard_repositories.dart
@@ -1,41 +1,93 @@
 // ignore_for_file: public_member_api_docs
 
 import 'dart:async';
+import 'dart:math';
 
 import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
 import 'package:rxdart/subjects.dart';
+import 'package:standard_repositories/standard_repositories.dart';
 
 typedef FromJson<T> = T Function(Map<String, dynamic> json);
 typedef ToJson<T> = Map<String, dynamic> Function(T object);
 typedef TestFunction<T> = bool Function(T object);
 
+typedef UniqueEvent<T> = ({T event, int id});
+
 abstract class Repository<T> {
   Repository({
+    required RepositoryCache<T> repositoryCacher,
     required T initialValue,
-  }) : _cache = BehaviorSubject.seeded(initialValue);
+    required this.fromJson,
+    required this.toJson,
+  })  : _random = Random(),
+        _cache = BehaviorSubject.seeded(
+          (event: initialValue, id: 0),
+        ),
+        _repositoryCacher = repositoryCacher {
+    _readValue().then((value) {
+      if (value != null) _cache.value = _createEvent(value);
+    });
+  }
 
-  T get value => _cache.value;
-  Stream<T> get stream => _cache.stream;
+  final Random _random;
+  final FromJson<T> fromJson;
+  final ToJson<T> toJson;
+  final RepositoryCache<T> _repositoryCacher;
 
-  final BehaviorSubject<T> _cache;
+  UniqueEvent<T> _createEvent(T data) => (
+        event: data,
+        id: _random.nextInt(1000),
+      );
+
+  T get value => _cache.value.event;
+  Stream<T> get stream => _cache.stream.map((e) => e.event);
+
+  final BehaviorSubject<UniqueEvent<T>> _cache;
 
   @protected
   Future<void> setValue(FutureOr<T> Function() create) async {
     try {
-      _cache.value = await create();
+      _cache.value = _createEvent(await create());
     } catch (e) {
       _cache.addError(e);
+    } finally {
+      unawaited(_writeValue(value));
+    }
+  }
+
+  Future<void> _writeValue(T value) async {
+    try {
+      await _repositoryCacher.writeValue(runtimeType.toString(), toJson(value));
+    } catch (_) {}
+  }
+
+  Future<T?> _readValue() async {
+    try {
+      final value = await _repositoryCacher.readValue(runtimeType.toString());
+      if (value == null) return null;
+      return fromJson(value);
+    } catch (_) {
+      return null;
     }
   }
 }
 
 abstract class MultiRepository<T> extends Repository<Iterable<T>> {
   MultiRepository({
+    required super.repositoryCacher,
+    required FromJson<T> fromJson,
+    required ToJson<T> toJson,
     super.initialValue = const {},
-  });
+  }) : super(
+          fromJson: (json) =>
+              List<Map<String, dynamic>>.from(json['list'] as List<dynamic>)
+                  .map(fromJson),
+          toJson: (list) => {'list': list.map((e) => toJson(e)).toList()},
+        );
 
   Stream<Iterable<T>> streamWhere(TestFunction<T> filter) => _cache.stream
+      .map((e) => e.event)
       .map((all) => all.where((e) => filter(e)))
       .where((e) => e.isNotEmpty);
 
@@ -58,6 +110,29 @@ abstract class MultiRepository<T> extends Repository<Iterable<T>> {
     await setValue(() async {
       final newValues = await create();
       return {...value, ...newValues};
+    });
+  }
+
+  @protected
+  void removeValue(
+    bool Function(T datum) selector,
+  ) {
+    setValue(() {
+      final temp = value.where((e) => !selector(e));
+      return temp;
+    });
+  }
+
+  @protected
+  Future<void> replaceValue(
+    bool Function(T datum) selector,
+    FutureOr<T> Function(T current) create,
+  ) async {
+    await setValue(() async {
+      final temp = value.where((e) => !selector(e));
+      final current = value.firstWhere(selector);
+      final newValue = await create(current);
+      return {...temp, newValue};
     });
   }
 }
--- packages/standard_repositories_bloc/lib/src/standard_repository_listener.dart
+++ packages/standard_repositories_bloc/lib/src/standard_repository_listener.dart
@@ -7,7 +7,7 @@ import 'package:standard_repositories/standard_repositories.dart';
 /// A mixin that simplifies listening to streams in Blocs and Cubits
 /// {@endtemplate}
 mixin StandardRepositoryListener<State> on BlocBase<State> {
-  final Set<StreamSubscription<dynamic>> _subscriptions = {};
+  final Map<String, StreamSubscription<dynamic>> _subscriptions = {};
 
   /// Listens to the [repository] and calls [onData] for every event.
   void mountRepository<T>(
@@ -19,27 +19,70 @@ mixin StandardRepositoryListener<State> on BlocBase<State> {
     Function? onSubscribe,
     bool Function(T data)? where,
   }) {
-    var stream = repository.stream;
-    if (where != null) {
-      stream = stream.where(where);
-    }
-    _subscriptions.add(
-      stream.listen(
+    final key = repository.runtimeType.toString();
+    if (!_subscriptions.containsKey(key)) {
+      var stream = repository.stream;
+      if (where != null) {
+        stream = stream.where(where);
+      }
+      _subscriptions[key] = stream.listen(
         onData,
         onError: onError,
         onDone: onDone,
         cancelOnError: cancelOnError,
-      ),
-    );
-    // ignore: avoid_dynamic_calls
-    onSubscribe?.call();
+      );
+      // ignore: avoid_dynamic_calls
+      onSubscribe?.call();
+    }
+  }
+
+  /// Listens to the [repository] and calls [onData] for every event after
+  /// transforming it with [selector].
+  ///
+  /// The [selector] function allows you to extract or transform specific data
+  /// from the repository's model before it is passed to [onData].
+  void mountRepositoryWithSelector<Model, Data>(
+    Repository<Model> repository, {
+    required Data Function(Model data) selector,
+    required void Function(Data data) onData,
+    void Function(Object error, StackTrace stackTrace)? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+    Function? onSubscribe,
+    bool Function(Model data)? where,
+  }) {
+    final key = repository.runtimeType.toString();
+    if (!_subscriptions.containsKey(key)) {
+      var stream = repository.stream;
+      if (where != null) {
+        stream = stream.where(where);
+      }
+      _subscriptions[key] = stream.map(selector).listen(
+            onData,
+            onError: onError,
+            onDone: onDone,
+            cancelOnError: cancelOnError,
+          );
+      // ignore: avoid_dynamic_calls
+      onSubscribe?.call();
+    }
+  }
+
+  /// Cancels any subscriptions to the repository
+  Future<void> unmountRepository(Repository<dynamic> repository) async {
+    final key = repository.runtimeType.toString();
+    if (_subscriptions.containsKey(key)) {
+      await _subscriptions[key]!.cancel();
+      _subscriptions.remove(key);
+    }
   }
 
   @override
   Future<void> close() {
-    for (final subscription in _subscriptions) {
+    for (final subscription in _subscriptions.values) {
       subscription.cancel();
     }
+    _subscriptions.clear();
     return super.close();
   }
 }
